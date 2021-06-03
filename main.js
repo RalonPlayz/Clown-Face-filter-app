928820a3c8cea624ea774932936a1fb6422a5a6d
RalonPlayz <79267114+RalonPlayz@users.noreply.github.com>
2021-06-03 16:50:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+noseX=0;
+noseY=0
+
 function preload()
 {
+    clown_nose = loadImage('https://i.postimg.cc/t4rBp3vH/clownnose.png');
 }
 
 function setup()
@@ -22,14 +26,20 @@ function modelLoaded()
     {
         if(results.length > 0){
             console.log(results);
-            console.log("nose x = " + results[0].pose.nose.x);
-            console.log("nose y = " + results[0].pose.nose.y);
+           noseX =  results[0].pose.nose.x-18;
+           noseY = results[0].pose.nose.y-18;
+           console.log("nose x = " + results[0].pose.nose.x);
+           console.log("nose y = " + results[0].pose.nose.y);
         }
     }
 
 function draw()
 {
     image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255,0 ,0 );
+    //circle(noseX, noseY, 20);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot()
